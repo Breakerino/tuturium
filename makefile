a7545c8299598bb246bf79984c5ace6751c6c0cd
Breakerino <git@breakerino.me>
2025-09-14 11:45:53 +0000

    Global: Add development and production runner scripts

---
+++ makefile
@@ -0,0 +1,31 @@
+.PHONY: start stop restart build force-build start-app-dev start-app-storybook build-app start-api-dev build-api
+
+start:
+	bash scripts/prod/start.sh
+	
+force-start:
+	bash scripts/prod/start.sh --force
+
+stop:
+	bash scripts/prod/stop.sh
+
+restart:
+	bash scripts/prod/restart.sh
+
+build:
+	bash scripts/prod/build.sh
+	
+force-build:
+	bash scripts/prod/build.sh --force
+
+start-app-dev:
+	bash scripts/dev/start-app.sh
+
+build-app:
+	bash scripts/dev/build-app.sh
+
+start-api-dev:
+	bash scripts/dev/start-api.sh
+
+build-api:
+	bash scripts/dev/build-api.sh
---
+++ scripts/prod/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Change directory to the project root (two levels up from this script's location).
+cd "$(dirname "$0")/../.."
+
+# Build the Strapi admin
+if [[ "$1" == "--force" ]]; then
+  docker compose run --rm api yarn build
+elif ! docker compose run --rm api test -d /srv/app/dist/build; then
+  docker compose run --rm api yarn build
+fi
+
+# Start all services defined in the local Docker Compose configuration in detached mode.
+docker compose --file docker-compose.yml --env-file .env up -d
