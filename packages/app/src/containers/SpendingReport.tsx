1a3e63b6ec99ea967422f248efb2b610b46394c4
Breakerino <git@breakerino.me>
2025-09-14 11:50:33 +0000

    App: SpendingReport - Extend functionality to support fetching most relevant data if spending date is not passed, also add loading state to spending month

--- packages/app/src/containers/SpendingReport.tsx
+++ packages/app/src/containers/SpendingReport.tsx
@@ -1,5 +1,5 @@
 //
-import { Flex, Grid, GridProps, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridProps, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useMediaQuery } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { AgChartOptions } from 'ag-charts-community';
 import { AgCharts } from 'ag-charts-react';
@@ -27,50 +27,61 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 	const navigate = useNavigate();
 	const [queryParams, setQueryParams] = useSearchParams();
 
-	const [spendingDate, setSpendingDate] = React.useState<string>(queryParams.has('date') ? queryParams.get('date') ?? moment().format('MM-YYYY') : moment().format('MM-YYYY'));
+	const [spendingDate, setSpendingDate] = React.useState<string | null>(queryParams.has('date') ? queryParams.get('date') ?? null : null);
 	const [spendingWeek, setSpendingWeek] = React.useState<number | null>(queryParams.has('week') ? Number(queryParams.get('week')) : null);
 	const [spendingType, setSpendingType] = React.useState<string>(queryParams.has('type') ? queryParams.get('type') ?? 'expense' : 'expense');
 	const [spendingTab, setSpendingTab] = React.useState<number>(queryParams.has('tab') ? Number(queryParams.get('tab')) ?? 0 : 0);
 
 	const [isTablet] = useMediaQuery('(min-width: 768px)');
-	
+
 	React.useEffect(() => {
 		// @ts-ignore
 		setQueryParams({
-			...(spendingDate ? {date: spendingDate}: {}),
-			...(spendingWeek ? {week: spendingWeek}: {}),
-			...(spendingType ? {type: spendingType} : {}),
-			...(spendingTab ? {tab: spendingTab} : {}),
+			...(spendingDate ? { date: spendingDate } : {}),
+			...(spendingWeek ? { week: spendingWeek } : {}),
+			...(spendingType ? { type: spendingType } : {}),
+			...(spendingTab ? { tab: spendingTab } : {}),
 		})
 	}, [spendingDate, spendingWeek, spendingType, spendingTab])
-
+	
 	const spendingReportQuery = useQuery<Record<string, any>>({
 		queryKey: ['spending-report', spendingDate, spendingWeek, wallet],
 		queryFn: async () => {
 			try {
-				const monthStart = moment(spendingDate, 'MM-YYYY').startOf('month');
-				const monthEnd = moment(spendingDate, 'MM-YYYY').endOf('month');
+				const params: Record<string, any> = {
+					from: null,
+					to: null,
+					...(wallet ? { wallet } : {})
+				}
 
-				const dateFrom = spendingWeek
-					? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').startOf('week').isBefore(monthStart)
-						? monthStart
-						: moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').startOf('week')
-					: monthStart;
+				if (spendingDate && spendingDate !== 'Invalid date') {
+					const monthStart = moment(spendingDate, 'MM-YYYY').startOf('month');
+					const monthEnd = moment(spendingDate, 'MM-YYYY').endOf('month');
 
-				const dateTo = spendingWeek
-					? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').endOf('week')
-					: monthEnd;
+					const dateFrom = spendingWeek
+						? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').startOf('week').isBefore(monthStart)
+							? monthStart
+							: moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').startOf('week')
+						: monthStart;
 
-				const { status, data: { data } } = await api.getSpendingReport({
-					params: {
-						from: dateFrom.format('YYYY-MM-DD'),
-						to: dateTo.format('YYYY-MM-DD'),
-						...(wallet ? { wallet } : {})
-					}
-				});
+					const dateTo = spendingWeek
+						? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').endOf('week')
+						: monthEnd;
+
+					params.from = dateFrom.format('YYYY-MM-DD');
+					params.to = dateTo.format('YYYY-MM-DD');
+				}
+
+				const { status, data: { data } } = await api.getSpendingReport({ params });
 
 				if (status !== 200) {
 					return [];
+				}
+
+				const newSpendingDate = moment(data.date.from).format('MM-YYYY');
+
+				if (newSpendingDate !== spendingDate) {
+					setSpendingDate(newSpendingDate);
 				}
 
 				return data;
@@ -122,7 +133,11 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 			],
 		} as AgChartOptions
 	}, [spendingReportQuery.data, spendingType]);
-
+	
+	const spendingMonth = React.useMemo(() => {
+		return moment(spendingReportQuery?.data?.date.from).format('MM-YYYY');
+	}, [spendingReportQuery?.data]);
+	
 	const transactionCategories = React.useMemo(() => {
 		return spendingReportQuery?.data?.categories?.[spendingType] ?? [];
 	}, [spendingReportQuery?.data, spendingType]);
@@ -139,7 +154,7 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 		updateSpendingWeek('reset');
 
 		setSpendingDate((prevDate) => {
-			const nextDate = moment(prevDate, 'MM-YYYY')[action === 'prev' ? 'subtract' : 'add'](1, 'month');
+			const nextDate = moment(spendingMonth, 'MM-YYYY')[action === 'prev' ? 'subtract' : 'add'](1, 'month');
 
 			if (nextDate.unix() > moment().unix()) {
 				return prevDate;
@@ -182,16 +197,16 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 		const dateTo = spendingWeek
 			? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').endOf('week')
 			: monthEnd;
-			
+
 		const url = new URL(`${window.location.origin}/transactions/category/${category.uid}`);
-		
+
 		url.searchParams.append('from', dateFrom.format('YYYY-MM-DD'))
 		url.searchParams.append('to', dateTo.format('YYYY-MM-DD'))
-		
-		if ( wallet ) {
+
+		if (wallet) {
 			url.searchParams.append('wallet', wallet);
 		}
-		
+
 		navigate(url.pathname + url.search);
 	}
 
@@ -208,23 +223,23 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 		const dateTo = spendingWeek
 			? moment(spendingDate, 'MM-YYYY').startOf('month').add(spendingWeek - 1, 'weeks').endOf('week')
 			: monthEnd;
-			
+
 		const url = new URL(`${window.location.origin}/products/category/${category.id}`);
-		
+
 		url.searchParams.append('from', dateFrom.format('YYYY-MM-DD'))
 		url.searchParams.append('to', dateTo.format('YYYY-MM-DD'))
-		
-		if ( wallet ) {
+
+		if (wallet) {
 			url.searchParams.append('wallet', wallet);
 		}
-		
+
 		navigate(url.pathname + url.search);
 	}
-	
+
 	const handleTabsChange = (index: number) => {
 		setSpendingTab(index);
 	}
-	
+
 	return (
 		<Grid
 			gridTemplateColumns={{ xl: "1fr 1fr" }}
@@ -233,16 +248,26 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 			{...props}
 		>
 			<Flex flexDirection="column" alignItems="center" justifyContent="center" p="3.6rem" gap="2rem" maxW="calc(100vw - (2 * var(--tuturium-space-mobile-spacing-content-x)) - 0.5rem)">
-				<Flex gap="1rem" alignItems="center">
-					<Button variant="circle-icon" icon="general.chevron-left" onClick={() => updateSpendingDate('prev')} />
-					<Text as="span" bg="purple.800" color="purple.200" fontSize="2rem" fontWeight={500} lineHeight={1} textAlign="center" px="3.2rem" py="1.25rem" borderRadius="lg">{moment(spendingDate, 'MM-YYYY').format('MMM YYYY')}</Text>
-					<Button variant="circle-icon" icon="general.chevron-right" onClick={() => updateSpendingDate('next')} />
-				</Flex>
+				{spendingReportQuery.isLoading || spendingReportQuery.isFetching ? (
+					<Flex gap="1rem" alignItems="center">
+						<Button variant="circle-icon" icon="general.chevron-left" isDisabled />
+						<Box minW="15.2rem" bg="purple.800" color="purple.200" fontSize="2rem" fontWeight={500} lineHeight={1} textAlign="center" px="3.2rem" py="1.25rem" borderRadius="lg">
+							<Spinner size="sm" />
+						</Box>
+						<Button variant="circle-icon" icon="general.chevron-right" isDisabled />
+					</Flex>
+				) : (
+					<Flex gap="1rem" alignItems="center">
+						<Button variant="circle-icon" icon="general.chevron-left" onClick={() => updateSpendingDate('prev')} />
+						<Text as="span" bg="purple.800" color="purple.200" fontSize="2rem" fontWeight={500} lineHeight={1} textAlign="center" px="3.2rem" py="1.25rem" borderRadius="lg">{moment(spendingMonth, 'MM-YYYY').format('MMM YYYY')}</Text>
+						<Button variant="circle-icon" icon="general.chevron-right" onClick={() => updateSpendingDate('next')} isDisabled={spendingDate === moment().format('MM-YYYY')} />
+					</Flex>
+				)}
 
 				<Flex gap="1rem" alignItems="center">
-					<Button variant="circle-icon" icon="general.chevron-left" onClick={() => updateSpendingWeek('prev')} isDisabled={isNull(spendingWeek) || spendingWeek === 0} />
+					<Button variant="circle-icon" icon="general.chevron-left" onClick={() => updateSpendingWeek('prev')} isDisabled={isNull(spendingWeek) || spendingWeek === 0 || (spendingReportQuery.isLoading || spendingReportQuery.isFetching)} />
 					<Text as="span" bg="purple.800" color="purple.200" fontSize="1.6rem" fontWeight={500} lineHeight={1} textAlign="center" px="2.4rem" py="1rem" borderRadius="lg">Week {spendingWeek}</Text>
-					<Button variant="circle-icon" icon="general.chevron-right" onClick={() => updateSpendingWeek('next')} isDisabled={spendingWeek === 4} />
+					<Button variant="circle-icon" icon="general.chevron-right" onClick={() => updateSpendingWeek('next')} isDisabled={spendingWeek === 4 || (spendingReportQuery.isLoading || spendingReportQuery.isFetching)} />
 				</Flex>
 
 				{spendingReportQuery.isLoading || spendingReportQuery.isFetching ? (
@@ -251,28 +276,30 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 					</Grid>
 				) : (
 					spendingChartOptions ? (
-						<AgCharts
-							style={{
-								width: (!isTablet ? '28rem' : '40rem'),
-								height: (!isTablet ? '28rem' : '40rem')
-							}}
-							options={spendingChartOptions}
-						/>
+						<>
+							<AgCharts
+								style={{
+									width: (!isTablet ? '28rem' : '40rem'),
+									height: (!isTablet ? '28rem' : '40rem')
+								}}
+								options={spendingChartOptions}
+							/>
+
+							<Switcher
+								data={[
+									{ id: 'expense', label: 'Expense', value: formatAmount(spendingReportQuery?.data?.totals?.expense ?? 0, 'EUR'), color: '#D7775B' },
+									{ id: 'income', label: 'Income', value: formatAmount(spendingReportQuery?.data?.totals?.income ?? 0, 'EUR'), color: '#B8DD55' }
+								]}
+								selected={spendingType}
+								onSelect={(type) => setSpendingType(type)}
+							/>
+						</>
 					) : (
 						<Grid minHeight={{ base: '28rem', md: '40rem' }} placeItems="center">
 							<Text fontSize="2.4rem">No data</Text>
 						</Grid>
 					)
 				)}
-
-				<Switcher
-					data={[
-						{ id: 'expense', label: 'Expense', value: formatAmount(spendingReportQuery?.data?.totals?.expense ?? 0, 'EUR'), color: '#D7775B' },
-						{ id: 'income', label: 'Income', value: formatAmount(spendingReportQuery?.data?.totals?.income ?? 0, 'EUR'), color: '#B8DD55' }
-					]}
-					selected={spendingType}
-					onSelect={(type) => setSpendingType(type)}
-				/>
 			</Flex>
 
 			<Flex flexDirection="column"
@@ -280,7 +307,7 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 				maxW="calc(100vw - (2 * var(--tuturium-space-mobile-spacing-content-x)) - 0.5rem)"
 			>
 				<Tabs colorScheme="purple" variant="spending-overview" height="100%" isFitted defaultIndex={spendingTab} onChange={handleTabsChange}>
-					<TabList overflowX="auto" position="sticky" top="-2.45rem" bg="purple.900" zIndex={1}> 
+					<TabList overflowX="auto" position="sticky" top="-2.45rem" bg="purple.900" zIndex={1}>
 						<Tab
 							fontSize={{ base: '1.6rem', lg: '1.8rem' }}
 							flex={{
@@ -342,7 +369,7 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 										</Grid>
 									) : (
 										transactionCategories?.map((item: { uid: string, name: string, total: number }) => (
-											<TransactionCategoryItem {...item} onClick={() => navigateToTransactionCategory(item)} 
+											<TransactionCategoryItem {...item} onClick={() => navigateToTransactionCategory(item)}
 												p="0.75rem 1.4rem"
 												borderRadius="sm"
 												cursor="pointer"
@@ -388,7 +415,7 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 										</Grid>
 									) : (
 										transactions?.map((item: Record<string, any>) => (
-											<TransactionItem {...item} 
+											<TransactionItem {...item}
 												p="0.75rem 1.4rem"
 												borderRadius="sm"
 												cursor="pointer"
@@ -434,8 +461,8 @@ const SpendingReport: React.FC<SpendingReportProps> = ({ wallet, ...props }) => 
 										</Grid>
 									) : (
 										productCategories?.map((item: { name: string, total: number }) => (
-											<ProductCategoryItem {...{ ...item, total: item.total * -1 }} 
-												onClick={() => navigateToProductCategory(item)} 
+											<ProductCategoryItem {...{ ...item, total: item.total * -1 }}
+												onClick={() => navigateToProductCategory(item)}
 												p="0.75rem 1.4rem"
 												borderRadius="sm"
 												cursor="pointer"
